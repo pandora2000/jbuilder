8c8d3999551202e7b6a528d7e586e93092841332
Tetsuri Moriya <tetsuri.moriya@gmail.com>
2014-05-23 02:02:33 +0900

    temporary commit

--- lib/jbuilder/jbuilder_template.rb
+++ lib/jbuilder/jbuilder_template.rb
@@ -125,6 +125,7 @@ class JbuilderHandler
   def self.call(template)
     # this juggling is required to keep line numbers right in the error
     %{__already_defined = defined?(json); json||=JbuilderTemplate.new(self); #{template.source}
+      File.open('/tmp/hoge', 'w') do |f| f.write(json.target!) end
       json.target! unless (__already_defined && __already_defined != "method")}
   end
 end
